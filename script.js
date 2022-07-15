6fd031e87dec0cd88ebab93d5dea18640eb3f040
Mateus Henrique Burda <matchoburda@Hotmail.com>
2022-07-15 16:03:06 -0300

    Inicio do script

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-document.getElementById("submit-button").onclick = function adiconarTarefa() {
-    
-}
 
-/*document.getElementById("nome-tarefa").value; */
+document.getElementById("submit-button").onclick = function adicionarTarefa(){
+    var novoItem = document.createElement("li");// variavel para criar o li que vai ter a tarefa
+    let inputTarefa = document.getElementById("nome-tarefa").value; // pega o valor do input e coloca como conteúdo da variavel
+    var nomeTarefa = document.createTextNode(inputTarefa); //insere o valor como texto dentro da variavel
+    novoItem.appendChild(nomeTarefa);//insere o texto dentro da tag <li>
+    var listaTarefas = document.getElementById("lista-tarefas");//Insere a tag como conteúdo da variavel
+    listaTarefas.appendChild(novoItem);//insere a tag com o conteúdo dentro do UL
+}
