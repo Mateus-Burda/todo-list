037f0c72d38d8be23872b946b96b589a1b22cec2
Mateus Henrique Burda <matchoburda@Hotmail.com>
2022-07-15 16:38:16 -0300

    Estilização básica

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 document.getElementById("submit-button").onclick = function adicionarTarefa(){
     var novoItem = document.createElement("li");// variavel para criar o li
     var inputTarefa = document.getElementById("nome-tarefa").value; // pega o valor do input e coloca como conteúdo da variavel
-    var nomeTarefa = document.createTextNode(inputTarefa); //transforma o valor do input como texto
+    var inputTarefaUpper = inputTarefa.toUpperCase();//transforma o texto para Caixa Alta
+    var nomeTarefa = document.createTextNode(inputTarefaUpper); //transforma o valor do input como texto
     var listaTarefas = document.getElementById("lista-tarefas");//Seleciona o UL
     novoItem.appendChild(nomeTarefa);//insere o texto dentro da tag <li>
     listaTarefas.appendChild(novoItem);//insere a tag com o conteúdo dentro do UL
