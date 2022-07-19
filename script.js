da710918a094c5255bd01655a78c1ec631775c0d
Mateus Henrique Burda <matchoburda@Hotmail.com>
2022-07-19 10:02:48 -0300

    Mudança estiilo e script

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-var contarTarefas = 0;
-document.getElementById("submit-button").onclick = function adicionarTarefa(){
-    contarTarefas += 1; console.log(contarTarefas);
-    var pegarInput = document.getElementById("nome-tarefa").value //pegar o input
-    var tranformarEmTexto = document.createTextNode(pegarInput);
-    var checkbox = document.createElement("input");checkbox.type = "checkbox";checkbox.className = "checkbox";checkbox.setAttribute("id","tarefa"+contarTarefas); // variavel para criar o input  e definir o tipo;
-    checkbox.appendChild(tranformarEmTexto);
-    var label = document.createElement("label");label.appendChild(tranformarEmTexto);label.setAttribute("for","tarefa"+contarTarefas);label.setAttribute("class","checkbox")
-    var divFazer = document.getElementById("fazer");
-    var breakRow = document.createElement("br");
+const inputElement = document.querySelector(".nova-tarefa-input");
+const addTarefaButton = document.querySelector(".nova-tarefa-button")
+const containerTarefas = document.querySelector(".container-tarefas");
 
-    divFazer.appendChild(checkbox);
-    divFazer.appendChild(label);
-    divFazer.appendChild(breakRow);
+addTarefaButton.onclick = function novaTarefa() {
+    const novoParagrafo = document.createElement("p");
+    novoParagrafo.innerText = inputElement.value;
+
+    const novoBotão = document.createElement("button");
+    novoBotão.innerText = "X";
+    novoBotão.classList.add("botao-tarefa")
+
+    const novoDiv = document.createElement("div");
+    novoDiv.classList.add("div-tarefa");
+    novoDiv.appendChild(novoBotão);
+    novoDiv.appendChild(novoParagrafo);
+
+    containerTarefas.appendChild(novoDiv);
 }
